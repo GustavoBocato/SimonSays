5a80d17b72ba2d3b25be8e599af189559a93b736
peserra <pedro.occulate@gmail.com>
2024-11-03 12:40:16 -0300

    ajustado um pouco a cor

--- window.hpp
+++ window.hpp
@@ -7,56 +7,57 @@
 
 class Window : public abcg::OpenGLWindow {
 protected:
-  // metodos implementados em window.cpp
-  void onCreate() override;
-  void onPaint() override;
-  void onPaintUI() override;
-  void onResize(glm::ivec2 const &size) override;
-  void onDestroy() override;
+    // metodos implementados em window.cpp
+    void onCreate() override;
+    void onPaint() override;
+    void onPaintUI() override;
+    void onResize(glm::ivec2 const &size) override;
+    void onDestroy() override;
 
 private:
-  // utilizado no onResize
-  glm::ivec2 m_viewportSize{};
+    // utilizado no onResize
+    glm::ivec2 m_viewportSize{};
 
-  /*
-  recursos alocados pelo OpenGL -> shaders que definem o comportamento da
-  renderização
+    /*
+    recursos alocados pelo OpenGL -> shaders que definem o comportamento da
+    renderização
 
-  */
+    */
 
-  GLuint m_vao{};
-  GLuint m_vboVertices{};
-  GLuint m_vboColors{};
-  GLuint m_program{};
-  abcg::Timer m_timer;
-  abcg::Timer m_turnTimer;
-  std::default_random_engine m_randomEngine;
+    GLuint m_vao{};
+    GLuint m_vboVertices{};
+    GLuint m_vboColors{};
+    GLuint m_program{};
+    abcg::Timer m_timer;
+    abcg::Timer m_turnTimer;
+    float const m_colorAtualizationRate = 1.0/4;
+    std::default_random_engine m_randomEngine;
 
-  // variaveis que a classe tem
-  // vamos pensar melhor nelas, juntos
+    // variaveis que a classe tem
+    // vamos pensar melhor nelas, juntos
 
-  enum class GameState { Play, Running, GameOver };
-  GameState m_gameState; // guarda em qual estado o jogo esta
+    enum class GameState { Play, Running, GameOver };
+    GameState m_gameState; // guarda em qual estado o jogo esta
 
-  // vira true quando player tem que dar input
-  bool m_isPlayerTurn{true};
+    // vira true quando player tem que dar input
+    bool m_isPlayerTurn{true};
 
-  // definindo cores padrao
-  inline static const glm::vec4 m_Red{1.00f, 0.00f, 0.00f, 1.00f};
-  inline static const glm::vec4 m_CRed{1.00f, 0.40f, 0.40f, 1.00f};
-  inline static const glm::vec4 m_Green{0.00f, 1.00f, 0.00f, 1.00f};
-  inline static const glm::vec4 m_CGreen{0.4f, 1.00f, 0.40f, 1.00f};
-  inline static const glm::vec4 m_Blue{0.00f, 0.00f, 1.00f, 1.00f};
-  inline static const glm::vec4 m_CBlue{0.40f, 0.40f, 1.00f, 1.00f};
-  inline static const glm::vec4 m_Yellow{1.00f, 1.00f, 0.00f, 1.00f};
-  inline static const glm::vec4 m_CYellow{1.00f, 1.00f, 0.60f, 1.00f};
-  inline static const glm::vec4 m_Black{0.00f, 0.00f, 0.00f, 1.00f};
+    // definindo cores padrao
+    inline static const glm::vec4 m_Red{1.00f, 0.00f, 0.00f, 1.00f};
+    inline static const glm::vec4 m_CRed{1.00f, 0.40f, 0.40f, 1.00f};
+    inline static const glm::vec4 m_Green{0.00f, 1.00f, 0.00f, 1.00f};
+    inline static const glm::vec4 m_CGreen{0.4f, 1.00f, 0.40f, 1.00f};
+    inline static const glm::vec4 m_Blue{0.00f, 0.00f, 1.00f, 1.00f};
+    inline static const glm::vec4 m_CBlue{0.40f, 0.40f, 1.00f, 1.00f};
+    inline static const glm::vec4 m_Yellow{1.00f, 1.00f, 0.00f, 1.00f};
+    inline static const glm::vec4 m_CYellow{1.00f, 1.00f, 0.45f, 1.00f};
+    inline static const glm::vec4 m_Black{0.00f, 0.00f, 0.00f, 1.00f};
 
-  // cria um array de cores, inicialmente vazio (cabe 50 cores)
-  // vamos preencher aleatoriamente com cores, ao inicio do jogo
-  std::vector<std::string> m_colorSequence{};
-  void setupModel(int sides, glm::vec4 red, glm::vec4 green, glm::vec4 blue,
-                  glm::vec4 yellow);
+    // cria um array de cores, inicialmente vazio (cabe 50 cores)
+    // vamos preencher aleatoriamente com cores, ao inicio do jogo
+    std::vector<std::string> m_colorSequence{};
+    void setupModel(int sides, glm::vec4 red, glm::vec4 green, glm::vec4 blue,
+                    glm::vec4 yellow);
 };
 
 #endif
